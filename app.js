8dfab9ca8ab1853989fa1cc9128d94b40935b4c3
WarunaUdara <warunaudarasam2003@gmail.com>
2024-03-27 03:55:30 +0530

    4th commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//weather App by Waruna Udara Sampath
+
+let name1=document.getElementById("text-box").value;
+const apiKeyOpenWeather="043c555d770214a59491f1082279d245";
+const apiKeyFreeWeather="4c56104522d74b8c980220542242603";
+
+
+
+function searchButton(){
+        name1=document.getElementById("text-box").value;
+        const cityName=name1;
+        console.log(cityName);
+}
+
