cd268586f06373acead4a7629a219ce4289f39ae
WarunaUdara <warunaudarasam2003@gmail.com>
2024-03-31 05:31:13 +0530

    7th commit

--- app.js
+++ app.js
@@ -62,9 +62,42 @@ function replaceSpacesWithPercent20(str) {
   return str.split(" ").join("%20");
 }
 
+function getCurrentPosition() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+      console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+  console.log("Latitude: " + position.coords.latitude);
+  console.log("Longitude: " + position.coords.longitude);
+}
+
+function showError(error) {
+  switch(error.code) {
+      case error.PERMISSION_DENIED:
+          console.log("User denied the request for Geolocation.");
+          break;
+      case error.POSITION_UNAVAILABLE:
+          console.log("Location information is unavailable.");
+          break;
+      case error.TIMEOUT:
+          console.log("The request to get user location timed out.");
+          break;
+      case error.UNKNOWN_ERROR:
+          console.log("An unknown error occurred.");
+          break;
+  }
+}
+
+
+
+
 fetch("http://api.weatherapi.com/v1/current.json?key=4c56104522d74b8c980220542242603&q=sri%20lanka")
         .then(response => response.json())
-        .then(data =>console.log(data) );
+        .then(data =>console.log(data.location.lat) );
 
 //data.current.temp_c
 //data.current.temp_f
